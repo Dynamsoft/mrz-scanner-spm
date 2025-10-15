4e09b4b8ce04b8659266149da94c7b68991347c3
DMGithubPublisher <github@dynamsoft.com>
2025-10-15 17:36:48 +0800

    update to 3.2.1000

--- Package.swift
+++ Package.swift
@@ -7,19 +7,23 @@ let package = Package(
     name: "DynamsoftMRZScanner",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "DynamsoftCaptureVisionBundle", targets: ["DynamsoftCaptureVisionBundle"]),
-        .library(name: "DynamsoftMRZScannerBundle", targets: ["DynamsoftMRZScannerBundle"]),
-        
+        .library(name: "DynamsoftMRZScanner", targets: ["DynamsoftMRZScanner"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.1000")
     ],
-    targets: [       
-        .binaryTarget(name: "DynamsoftCaptureVisionBundle", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-capturevision-bundle-ios-3.0.5200-xcframework.zip", checksum: "9a0cd5b63e7846603b49d88c6703b378f4d3c44e44a56348a21cb6064d4b4ba7"),
-        .binaryTarget(name: "DynamsoftMRZScannerBundle", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-mrzscanner-bundle-ios-3.0.5200-xcframework.zip", checksum: "66b4a45e4980913d656846782e1239c208b4828b5436c0c348c3cd7a61ab86ac"),
-        
+    targets: [
+        .binaryTarget(
+            name: "DynamsoftMRZScannerBundle", 
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-mrzscanner-bundle-ios-3.2.1000-xcframework.zip", 
+            checksum: "fc288c4f6998d320cb0e9a2904e984403eaad4fcab083d93cc9fa32fd4244eeb"
+        ),
+        .target(
+            name: "DynamsoftMRZScanner",
+            dependencies: [
+                "DynamsoftMRZScannerBundle",
+                .product(name: "DynamsoftCaptureVisionBundle", package: "capture-vision-spm")
+            ],
+        )
     ]
 )
-
-
-
-
