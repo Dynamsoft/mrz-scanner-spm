050f0e404bca74efba5fdaedcc6ac5e83eb67986
DMGithubPublisher <github@dynamsoft.com>
2025-12-16 13:46:16 +0800

    update to 3.2.5000

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .library(name: "DynamsoftMRZScanner", targets: ["DynamsoftMRZScanner"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.3000")
+        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.5000")
     ],
     targets: [
         .binaryTarget(
             name: "DynamsoftMRZScannerBundle", 
-            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-mrzscanner-bundle-ios-3.2.3000-xcframework.zip", 
-            checksum: "d742ce207ed42f30c96a6a5381f4bb1b664afc855916ab1ada612f0e5c1aeee9"
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-mrzscanner-bundle-ios-3.2.5000-xcframework.zip", 
+            checksum: "84e27cee5f1623d402840052eec9643e037d079135b3a5ad5162841ca0e1321c"
         ),
         .target(
             name: "DynamsoftMRZScanner",
